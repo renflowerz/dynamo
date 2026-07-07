43595f179d8ca44d839fa9cf4d36bd6d9a9c19d0
ashnamehrotra <ashnamehrotra@gmail.com>
2026-07-07 13:29:00 -0400

    test(profiler): treat mounted model paths as local in mocks
    Signed-off-by: ashnamehrotra <ashnamehrotra@gmail.com>

--- components/src/dynamo/profiler/tests/integration/test_profile_sla_dgdr.py
+++ components/src/dynamo/profiler/tests/integration/test_profile_sla_dgdr.py
@@ -496,8 +496,13 @@ def _run_mocked_thorough(dgdr, ops, backend: str):
     """Run the full mocked-thorough pipeline for an arbitrary backend."""
     thorough_patches = _make_thorough_patches(backend)
     kv_patch = _patch_kv_cache_log(backend)
+    trust_patch = patch(
+        "dynamo.profiler.utils.config_modifiers.protocol."
+        "model_ref_allows_implicit_trust_remote_code",
+        side_effect=lambda model_ref: str(model_ref).startswith("/"),
+    )
 
-    with kv_patch:
+    with kv_patch, trust_patch:
         for p in thorough_patches:
             p.start()
         try:
